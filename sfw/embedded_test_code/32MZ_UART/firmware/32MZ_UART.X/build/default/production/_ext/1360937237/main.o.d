3aa6ae6a0842bdabd6e960d5dce9d4f86aa29042
Drew <drewmaatman2gmail.com>
2018-10-11 05:55:04 -0500

    Adding embedded test programs as of 10/11

---
+++ sfw/embedded_test_code/32MZ_UART/firmware/32MZ_UART.X/build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1360937237/main.o:  \
+../src/main.c  \
+ ../../../../../../../microchip/harmony/v2_06/framework/system/common/sys_module.h  \
+ ../../../../../../../microchip/harmony/v2_06/framework/system/common/sys_common.h 
---
+++ sfw/embedded_test_code/32MZ_UART/firmware/32MZ_UART.X/build/default/production/_ext/340578644/sys_devcon_cache_pic32mz.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/340578644/sys_devcon_cache_pic32mz.o:  \
+ ../src/system_config/default/framework/system/devcon/src/sys_devcon_cache_pic32mz.S  \
+ ../src/system_config/default/framework/system/devcon/src/sys_devcon_cache.h 
+build/default/production/_ext/340578644/sys_devcon_cache_pic32mz.o:  \
+ ../src/system_config/default/framework/system/devcon/src/sys_devcon_cache.h  \
+  \
+ ../src/system_config/default/framework/system/devcon/src/sys_devcon_cache_pic32mz.S  \
