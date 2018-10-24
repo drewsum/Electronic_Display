3c067dcbbc50b0020d185ac6649b171af4d48114
kevinetta <kevin.etta@marquette.edu>
2018-10-24 03:33:51 -0500

    added basic content

---
+++ sfw/app/LED_Display/app/src/main/java/display/led_display/HomeActivity.java
@@ -0,0 +1,13 @@
+package display.led_display;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
