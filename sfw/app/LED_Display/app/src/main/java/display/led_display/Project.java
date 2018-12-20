252f1a3bd3631beb156d4924019e933340caec41
kevinetta <kevin.etta@marquette.edu>
2018-12-20 12:33:27 -0600

    outline for some objects

--- sfw/app/LED_Display/app/src/main/java/display/led_display/Project.java
+++ sfw/app/LED_Display/app/src/main/java/display/led_display/Project.java
@@ -1,5 +1,22 @@
 package display.led_display;
 
+import java.security.Timestamp;
+import java.util.List;
+
 public class Project {
-    
+    public String projectName;
+    public Timestamp changeDate;
+    public enum projectTypes {
+        CUSTOM,
+        POWERPOINT
+    };
+    public List<Frame> frameList;
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
 }
