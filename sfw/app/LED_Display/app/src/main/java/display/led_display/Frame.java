a5c40ed11e04a134f35d3ab916c27c611f89babc
kevinetta <kevin.etta@marquette.edu>
2018-12-20 12:33:27 -0600

    outline for some objects

---
+++ sfw/app/LED_Display/app/src/main/java/display/led_display/Frame.java
@@ -0,0 +1,9 @@
+package display.led_display;
+
+public class Frame {
+    public enum frameType {
+        Image,
+        Text
+    };
+    public byte[] frameData;
+}
---
+++ sfw/app/LED_Display/app/src/main/java/display/led_display/PhysicalBoard.java
@@ -0,0 +1,8 @@
+package display.led_display;
+
+public class PhysicalBoard {
+    public String boardName;
+    public int[] ipAddress;
+    public int portNumber;
+    public int pinNumber;
+}
