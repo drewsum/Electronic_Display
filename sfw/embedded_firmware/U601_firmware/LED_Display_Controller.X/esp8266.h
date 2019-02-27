a6daca0661bd92d439472899724709c3be0f261a
kevinetta <kevin.etta@marquette.edu>
2019-02-27 13:44:00 -0600

    First pass at reusing USB_UART code

--- sfw/embedded_firmware/U601_firmware/LED_Display_Controller.X/esp8266.h
+++ sfw/embedded_firmware/U601_firmware/LED_Display_Controller.X/esp8266.h
@@ -17,6 +17,40 @@
 #define _ESP8266_H
 
 #include <xc.h>
+// These are macros needed for defining ISRs, included in XC32
+#include <sys/attribs.h>
+
+// Sizes of TX and RX ring buffers
+#define ESP_8266_TX_BUFFER_SIZE 16384
+#define ESP_8266_RX_BUFFER_SIZE 2048
+
+char esp_8266_line[ESP_8266_RX_BUFFER_SIZE];
+
+// This function initializes UART 6 for USB debugging
+void esp8266Initialize(void);
+
+// These are the USB UART Interrupt Service Routines
+void __ISR(_UART1_RX_VECTOR, ipl2SRS) esp8266ReceiveISR(void);
+void __ISR(_UART1_TX_VECTOR, ipl7SRS) esp8266TransferISR(void);
+void __ISR(_UART1_FAULT_VECTOR, ipl1SRS) esp8266FaultISR(void);
+
+// This function allows reading of a byte from UART1
+uint8_t esp8266ReadByte(void);
+
+// This function places a character into the output stream of UART1
+void esp8266Putchar(uint8_t txData);
+
+// These are the UART1 interrupt handlers
+void esp8266TransmitHandler(void);
+void esp8266ReceiveHandler(void);
+
+// Ring buffer interface functions
+void esp8266RingBufferPull(void);
+void esp8266RingBufferLUT(char * esp_8266_line);
+
+// WiFi specific functions
+void sendCIPData(uint8_t connectionId, uint8_t * data, uint8_t length);
+void sendHTTPResponse(uint8_t connectionId, uint8_t * content, uint8_t length);
 
 #endif /* _ESP8266_H */
 
