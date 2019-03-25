f6ca4cb7c72b86893a061985e8614cd748a23acf
cmgilger1 <cmgilger1@gmail.com>
2019-03-25 17:41:48 -0500

    change notification pins and interrupt handlers

--- sfw/embedded_firmware/U601_firmware/LED_Display_Controller.X/misc_board_control.h
+++ sfw/embedded_firmware/U601_firmware/LED_Display_Controller.X/misc_board_control.h
@@ -18,6 +18,40 @@
 
 #include <xc.h>
 
+#include <sys/attribs.h>
+
+// Initialize change notifications
+void changeNotificationInit(void);
+
+// Port F ISR
+void __ISR(_CHANGE_NOTICE_F_VECTOR, IPL2SRS) portFCNISR(void);
+
+// Port K ISR
+void __ISR(_CHANGE_NOTICE_K_VECTOR, IPL2SRS) portKCNISR(void);
+
+// POS5P Thermal Warning interrupt handler
+void pos5pThermalWarningHandler(void);
+
+// POS12 PGOOD interrupt handler
+void pos12PGoodHandler(void);
+
+// POS5 PGOOD interrupt handler
+void pos5PGoodHandler(void);
+
+// POS5P PGOOD interrupt handler
+void pos5pPGoodHandler(void);
+
+// Display enable interrupt handler
+void displayEnableHandler(void);
+
+// Encoder step interrupt handler
+void encoderStepHandler(void);
+
+// POS3P3 PGOOD interrupt handler
+void pos3p3PGoodHandler(void);
+
+
+
 #endif /* _MISC_BOARD_CONTROL_H */
 
 /* *****************************************************************************
